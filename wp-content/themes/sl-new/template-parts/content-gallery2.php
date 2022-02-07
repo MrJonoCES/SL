90243ea76bea71fd09f72256f63a7f1eaf87fb3d
Jono L <contact@cityeditionstudio.co.uk>
2022-02-07 17:23:28 +0000

    fixed gallery type 2

---
+++ wp-content/themes/sl-new/template-parts/content-gallery2.php
@@ -0,0 +1,17 @@
+<div class="gallery2 container center">
+  <?php $images = get_sub_field('gallery_2'); ?>
+  <?php foreach($images as $image) : ?> 
+
+    <div class="gallery-image mb4 ph3 <?php the_field('image_width', $image['id']); ?> <?php the_field('grid_image_position', $image['id']); ?>">
+
+    <?php echo wp_get_attachment_image($image['id'], 'full'); ?>
+
+    </div>
+  <?php endforeach; ?>
+  
+  <div class="text-block f4 ph3" style="align-self:end;">
+		<p><?php the_sub_field('gallery_text_block'); ?></p>
+	</div>
+</div>
+
+
